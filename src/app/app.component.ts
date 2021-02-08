d249cf575266440849f8cbb576962be5e582aa6d
KadoTomohiro <>
2021-02-08 18:28:13 +0900

    create polyhedron component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,71 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+    <app-polyhedron
+      [vertices]="verticesData"
+      [edges]="edgesData"
+      [faces]="facesData"
+      [colors]="colorData"></app-polyhedron>
+
+    <app-polyhedron
+      [vertices]="verticesData"
+      [edges]="starEdgesData"
+      [faces]="starFacesData"
+      [colors]="colorData"></app-polyhedron>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
   title = 'x3domExample';
+
+  au = (1 + Math.sqrt(5)) / 2;
+  powAu = Math.pow(this.au, 2);
+
+  verticesData = [
+    [-1, this.powAu, 0], [1, this.powAu, 0], [-1, -this.powAu, 0], [1, -this.powAu, 0],
+    [-this.powAu, 0, -1], [-this.powAu, 0, 1], [this.powAu, 0, -1], [this.powAu, 0, 1],
+    [0, 1, -this.powAu], [0, -1, -this.powAu], [0, 1, this.powAu], [0, -1, this.powAu],
+    [-this.au, this.au, -this.au], [-this.au, -this.au, -this.au], [this.au, this.au, -this.au], [this.au, -this.au, -this.au],
+    [-this.au, this.au, this.au], [-this.au, -this.au, this.au], [this.au, -this.au, this.au], [this.au, this.au, this.au]
+  ];
+
+  facesData = [
+    [0, 1, 19, 10, 16], [1, 14, 6, 7, 19], [6, 15, 3, 18, 7],
+    [3, 2, 17, 11, 18], [17, 5, 16, 10, 11], [19, 7, 18, 11, 10],
+    [14, 8, 9, 15, 6], [1, 0, 12, 8, 14], [0, 16, 5, 4, 12],
+    [4, 5, 17, 2, 13], [9, 13, 2, 3, 15], [8, 12, 4, 13, 9]
+  ];
+  edgesData = [
+    [0, 1], [0, 12], [0, 16], [1, 14], [1, 19],
+    [2, 3], [2, 13], [2, 17], [3, 15], [3, 18],
+    [4, 5], [4, 12], [4, 13], [5, 16], [5, 17],
+    [6, 7], [6, 14], [6, 15], [7, 18], [7, 19],
+    [8, 9], [8, 12], [8, 14], [9, 13], [9, 15],
+    [10, 11], [10, 16], [10, 19], [11, 17], [11, 18]
+  ];
+
+  colorData = [0.21568627450980393, 0.49411764705882355, 0.7215686274509804]
+
+
+    starFacesData = [
+      [0, 2, 19, 4, 18], [1, 17, 6, 16, 3], [0, 18, 8, 10, 15],
+      [16, 6, 4, 19, 9], [1, 17, 8, 10, 13], [5, 7, 12, 11, 14],
+      [5, 7, 13, 10, 15], [2, 19, 9, 11, 14], [1, 13, 7, 12, 3],
+      [0, 2, 14, 5, 15], [16, 3, 12, 11, 9], [8, 17, 6, 4, 18]
+    ];
+    starEdgesData = [
+      [0, 2], [0, 15], [0, 18], [1, 3], [1, 13],
+      [1, 17], [2, 14], [2, 19], [3, 12], [3, 16],
+      [4, 6], [4, 18], [4, 19], [5, 7], [5, 14],
+      [5, 15], [6, 16], [6, 17], [7, 12], [7, 13],
+      [8, 10], [8, 17], [8, 18], [9, 11], [9, 16],
+      [9, 19], [10, 13], [10, 15], [11, 12], [11, 14]
+      // [0, 2], [2, 19], [19, 4], [4, 18], [18, 0],
+      // [1, 3], [3, 16], [16, 6], [6, 17], [17, 1],
+      // [4, 5], [4, 12], [4, 13], [5, 16], [5, 17],
+      // [6, 7], [6, 14], [6, 15], [7, 18], [7, 19],
+      // [8, 9], [8, 12], [8, 14], [9, 13], [9, 15],
+      // [10, 11], [10, 16], [10, 19], [11, 17], [11, 18]
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PolyhedronComponent } from './polyhedron/polyhedron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolyhedronComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
